1a809762bd7a9f6b9e43468ade6284b1ec831fea
Sagar R Shivappa <sagar.rs@globant.com>
2024-10-17 18:11:44 +0530

    solution updated

--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,6 +1,6 @@
 async function Errorhandler(err, req, res, next) {
   //write the logic to send the error status and error message
-  res.status(err.status || 500).json({
+  res.status(501).json({
     status: "error",
     message: err.message || "Internal Server Error",
   });
